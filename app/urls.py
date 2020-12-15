6d82300b89c06213729e487eff70a7a164e44a24
Hardik Karena <52252350+hardikkarena@users.noreply.github.com>
2020-12-15 22:11:54 +0530

    LogIn Registration templates Intigrate

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path,include
+from . import views
 
 urlpatterns = [
-    
+    path("",views.Showhome,name="home"),
+     path("login/",views.Login,name="login"),
+     path("register/",views.Register,name="register"),
+     
 ]
