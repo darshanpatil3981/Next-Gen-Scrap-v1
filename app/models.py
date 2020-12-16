2be0f1ca36875365a9b61dfb1a870359095dcd2b
Darshan Patil <71498417+darshanpatil3981@users.noreply.github.com>
2020-12-16 15:15:56 +0530

    Basic Tables Created with MySQL

--- app/models.py
+++ app/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+
+class User_Master(models.Model):
+    Email = models.EmailField(max_length=50)
+    Password = models.CharField(max_length=50)
+    Role = models.CharField(max_length=20)
+    Otp = models.IntegerField()
+    is_created = models.DateTimeField(auto_now=True,blank=False)
+    is_verified = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_updated = models.DateTimeField(auto_now=True,blank=False)
+
+class Customer(models.Model):
+    Customer_ID = models.ForeignKey(User_Master,on_delete=models.CASCADE)
+    Firstname = models.CharField(max_length=30)
+    Lastname = models.CharField(max_length=30)
+    Address = models.CharField(max_length=100)
+    City = models.CharField(max_length=30)
+    State = models.CharField(max_length=30)
+    Pincode = models.BigIntegerField()
+    Contact = models.BigIntegerField(default=0)
+    Profile_Pic = models.ImageField(upload_to='app/Profile_Pics')
+
+class GC(models.Model):
+    GC_ID = models.ForeignKey(User_Master,on_delete=models.CASCADE)
+    Firstname = models.CharField(max_length=30)
+    Lastname = models.CharField(max_length=30)
+    Address = models.CharField(max_length=100)
+    City = models.CharField(max_length=30)
+    State = models.CharField(max_length=30)
+    Pincode = models.BigIntegerField()
+    Contact = models.BigIntegerField(default=0)
+    Profile_Pic = models.ImageField(upload_to='app/Profile_Pics')
+
+class RC(models.Model):
+    RC_ID = models.ForeignKey(User_Master,on_delete=models.CASCADE)
+    Firstname = models.CharField(max_length=30)
+    Lastname = models.CharField(max_length=30)
+    Address = models.CharField(max_length=100)
+    City = models.CharField(max_length=30)
+    State = models.CharField(max_length=30)
+    Pincode = models.BigIntegerField()
+    Contact = models.BigIntegerField(default=0)
+    Profile_Pic = models.ImageField(upload_to='app/Profile_Pics')
