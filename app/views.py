2f30f472d842b477b16270f541d36127bb78a83d
Darshan Patil <71498417+darshanpatil3981@users.noreply.github.com>
2021-01-18 23:02:58 +0530

    signup success thanks to hekur hardik

--- app/views.py
+++ app/views.py
@@ -12,21 +12,6 @@ def Login_Page(request):
 def Signup_as(request):
     return render(request,"app/signup_as.html")
 
-def Enter_email(request):
-    role = request.POST['roleh']
-    if role == "customer":
-        return render(request,"app/enter_email.html",{'role':role})
-    elif role == "business":
-        return render(request,"app/enter_email.html",{'role':role})
-
-    # if key==1:
-    #     message='1'
-    # elif key==2:
-    #     message='2'
-    # else:
-    #     message = "Something went wrong."
-    # return render(request,"app/enter_email.html",{'msg':message})  
-
 def Signup_customer(request):
     return render(request,"app/signup_customer.html")
 
@@ -39,6 +24,14 @@ def Otp_verification(request):
 
 
 # ====================================Processing Views=================================================
+
+def Enter_email(request):
+    role = request.POST['roleh']
+    if role == "customer":
+        return render(request,"app/enter_email.html",{'role':role})
+    elif role == "business":
+        return render(request,"app/enter_email.html",{'role':role})
+
 
 def is_already_created(request):
     role = request.POST['roleh']
@@ -76,7 +69,6 @@ def verify_OTP(request,sotp):
     if sotp==cotp:
         if role=="Customer":
             return render(request,"app/signup_customer.html",{'EMAIL':email})
-        #if role=="GCRC":
         else:
             return render(request,"app/signup_gcrc.html",{'EMAIL':email})
     else:
@@ -94,22 +86,13 @@ def create_Customer(request):
 
     if fname=="" or lname=="" or email=="" or pswd=="" or cpswd=="":
             message = "Please fill all details carefully..." 
-            return render(request,"app/signup_customer.html",{'msg':message})
+            return render(request,"app/signup_customer.html",{'msg':message,'EMAIL':email})
     
-    elif fname!="" or lname!="" or email!="" or pswd!="" or cpswd!="":
-#        isCraeatedAlready = User_Master.objects.filter(Email=email)
-#        if isCraeatedAlready:
-#            message = "This acoount is already created"
-#            return render(request,"app/signup_customer.html",{'msg':message})
-#        elif pswd==cpswd:
+    elif fname!="" or lname!="" or email!="" or pswd!="" or cpswd!="":      
             if pswd==cpswd:
-                #otp = randint(100000,999999)
                 newUser = User_Master.objects.create(Email=email,Password=pswd,Role="Customer",Otp=12345,is_created=True,is_verified=False,is_active=False,is_updated=False)
                 newCustomer = Customer.objects.create(Customer_ID=newUser,Firstname=fname,Lastname=lname,Address="",City="",State="",Pincode=000000,Contact=0,Profile_Pic="")
-                #email_Subject = "Customer Verification Mail"
-                #sendmail(email_Subject,'otpVerification_emailTemplate',email,{'name':fname,'otp':otp})
-                #return render(request,"app/otp_verification.html",{'obj':newUser})
-                return render(request,"app/index.html")
+             
             else:
                 message = "Password Doesnot match"
                 return render(request,"app/signup_customer.html",{'msg':message,'EMAIL':email})
